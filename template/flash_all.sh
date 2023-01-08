99ea2a44cf37d916ef0d84088d6e5ad018684235
Bardia Moshiri <fakeshell@bardia.tech>
2023-01-08 15:04:51 -0500

    strip the flash all script completely!

--- template/flash_all.sh
+++ template/flash_all.sh
@@ -1,77 +1,11 @@
 #!/bin/bash
-#
-# Flasher script for Droidian images
-# Copyright (C) 2022 Eugenio "g7" Paolantonio <me@medesimo.eu>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the <organization> nor the
-#      names of its contributors may be used to endorse or promote products
-#      derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-set -e
+fastboot flash boot data/boot.img
+fastboot flash dtbo data/dtbo.img
+fastboot flash vbmeta data/vbmeta.img
+fastboot flash vendor data/vendor.img
+fastboot flash userdata data/userdata.img
 
-info() {
-	echo "I: ${@}"
-}
-
-warning() {
-	echo "W: ${@}" >&2
-}
-
-error() {
-	echo "E: ${@}" >&2
-	exit 1
-}
-
-########################################################################
-
-flash() {
-	DEVICE="${1}"
-	IMAGE="${2}"
-	shift
-	shift
-	PARTITIONS="${@}"
-
-	[ -e "${IMAGE}" ] || error "${IMAGE} not found. Please check your downloaded archive"
-
-	info "Flashing ${IMAGE}"
-	for partition in ${PARTITIONS}; do
-		fastboot -s ${DEVICE} flash ${partition} ${IMAGE}
-	done
-}
-
-flash_if_exists() {
-	IMAGE="${2}"
-
-	[ -e "${IMAGE}" ] && flash ${@} || true
-}
-
-########################################################################
-
-flash ${DEVICE} data/boot.img boot
-flash_if_exists ${DEVICE} data/dtbo.img dtbo
-flash_if_exists ${DEVICE} data/vbmeta.img vbmeta
-flash_if_exists ${DEVICE} data/vendor.img vendor
-flash ${DEVICE} data/userdata.img userdata
-
-fastboot -s ${DEVICE} reboot
+fastboot reboot
 
 info "Flashing completed"

a5d46de3af7e16733179e203fa188db642e4291b
vacuumbeef <51813411+vacuumbeef@users.noreply.github.com>
2023-05-21 14:15:54 +0300

    Update flash_all.sh

--- template/flash_all.sh
+++ template/flash_all.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 fastboot flash boot data/boot.img
-fastboot flash dtbo data/dtbo.img
-fastboot flash vbmeta data/vbmeta.img
-fastboot flash recovery data/recovery.img
+fastboot flash vendor data/vendor.img
 fastboot flash userdata data/userdata.img
 
 fastboot reboot

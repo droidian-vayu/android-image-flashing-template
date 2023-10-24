71c87e52ace5e36e2e29b47d5b057e4d41b79fa8
vacuumbeef <51813411+vacuumbeef@users.noreply.github.com>
2023-10-24 10:11:53 +0300

    Update flash_all.bat - addedd flash vendor

--- template/flash_all.bat
+++ template/flash_all.bat
@@ -1,6 +1,7 @@
 fastboot flash boot data/boot.img
 fastboot flash dtbo data/dtbo.img
 fastboot flash vbmeta data/vbmeta.img
+fastboot flash vendor data/vendor.img
 fastboot flash userdata data/userdata.img
 
 fastboot reboot
